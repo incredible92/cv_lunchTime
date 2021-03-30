220652e5072829bcf8de49e0c1135b974e868595
incredible92 <sulymanabdullahi@gmail.com>
2021-03-30 17:21:49 +0000

    feat: CV_item8

---
+++ app.js
@@ -0,0 +1,30 @@
+const {WebClient} = require('@slack/web-api');
+const {createEventAdapter} = require('@slack/events-api');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+const botToken = process.env.BOT_TOKEN;
+const port = process.env.SLACK_PORT || 3000;
+
+
+const slackEvents = createEventAdapter(slackSigningSecret);
+const slackClient = new WebClient(botToken)
+
+
+slackEvents.on('app_mention', (e) => {
+    console.log(`Got message from villagers ${e.user}: ${e.text}`);
+    
+    (async () => {
+        try {
+         await slackClient.chat.postMessage({ channel: e.channel, text: `your order received!<@${e.user}>!`})
+        } catch (error) {
+            console.log(error.data)
+        }
+    })();
+})
+slackEvents.on('error', console.error);
+slackEvents.start(port).then(() => {
+    console.log(`server started on ${port}`)
+})
