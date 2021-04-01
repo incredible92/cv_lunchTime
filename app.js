16632c7dbb0f40b14371702dcecb2eb834e141e5
incredible92 <sulymanabdullahi@gmail.com>
2021-04-01 18:06:13 +0100

    fix: port error

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
 const express = require('express');
-
-const app = express()
-
 const {WebClient} = require('@slack/web-api');
 const {createEventAdapter} = require('@slack/events-api');
 
 const dotenv = require('dotenv');
 dotenv.config();
 
+const port = process.env.PORT || 3000;
+const app = express();
+
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 const botToken = process.env.BOT_TOKEN;
-const port = process.env.PORT || 3000;
-
 
 const slackEvents = createEventAdapter(slackSigningSecret);
-const slackClient = new WebClient(botToken)
+const slackClient = new WebClient(botToken);
 
 app.get('/', (req, res) => {
 res.send('place your order NOW!!!')
 })
+
 slackEvents.on('app_mention', (e) => {
     console.log(`Got message from villagers ${e.user}: ${e.text}`);
     
     (async () => {
         try {
-         await slackClient.chat.postMessage({ channel: e.channel, text: `your order received!<@${e.user}>!`})
+         await slackClient.chat.postMessage({ channel: e.channel, text: `your order received!<@${e.user}>!`});
         } catch (error) {
-            console.log(error.data)
+            console.log(error.data);
         }
     })();
 })
 slackEvents.on('error', console.error);
-slackEvents.start(port).then(() => {
-    console.log(`server started on ${port}`)
-})
+app.listen(port, () => {
+    console.log(`server started on ${port}`);
+});
